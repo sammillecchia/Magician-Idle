eefa4ce851316231ea28a7423f0d232a7efda59b
sammillecchia <sammillecchia@gmail.com>
2025-07-29 12:27:31 -0400

    1 - Addition of old code to be fixed

---
+++ old/utils/draggable.js
@@ -0,0 +1,46 @@
+//ignore below, for later features
+
+//AI Draggable function
+//Needs bounding, makes bars when you drag it offscreen
+//Position should also be done with vw/vh so resizing works
+//Also, I may want to change the z-index of currently dragged element
+//its probably a good idea to move this into another file/class
+//This would also be so it could manage these types of objects better
+//since I'm going to be using this to create menus it would also probably 
+//be helpful to add in creation/deletion of these elements to that same file.
+
+export function makeDraggable(element) {
+            let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+            element.onmousedown = dragMouseDown;
+
+            function dragMouseDown(e) {
+                e = e || window.event;
+                e.preventDefault();
+                // get the mouse cursor position at startup:
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                document.onmouseup = closeDragElement;
+                // call a function whenever the cursor moves:
+                document.onmousemove = elementDrag;
+            }
+
+            function elementDrag(e) {
+                e = e || window.event;
+                e.preventDefault();
+                // calculate the new cursor position:
+                pos1 = pos3 - e.clientX;
+                pos2 = pos4 - e.clientY;
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                // set the element's new position:
+                element.style.top = (element.offsetTop - pos2) + "px";
+                element.style.left = (element.offsetLeft - pos1) + "px";
+            }
+
+            function closeDragElement() {
+                /* stop moving when mouse button is released:*/
+                document.onmouseup = null;
+                document.onmousemove = null;
+            }
+}
+
