a9b84f60b82cb355bd62ffe045c93bde6ed93289
sammillecchia <sammillecchia@gmail.com>
2025-07-31 10:07:54 -0400

    3 - Added old modules and main.js to be refactored

--- js/main.js
+++ js/main.js
@@ -1,1 +1,124 @@
 //Main file
+
+import updateProgressBar from './utils/progressbar.js';
+import { setupAwakening } from "./skills/cultivation.js"
+import { setupTraining, updateText } from "./skills/training.js"
+import { magicalPower, addMagicalPower, setMagicalPower } from './player/stats.js';
+import { makeDraggable } from './utils/draggable.js';
+
+makeDraggable(document.getElementById('statsDisplay'));
+
+
+let currentMenu = trainingMenu
+
+export function displayMenu(menu) {
+    currentMenu.style.display = "none";
+    menu.style.display = "flex";
+    currentMenu = menu;
+}
+
+
+const body = document.body;
+
+
+function setup() {
+    setupAwakening();
+    setupTraining();
+}
+
+let mainInterval = setInterval(main, 50);
+
+let setupRun = 0;
+function main() {
+    checkMilestones();
+    updateText(); // Updates all text on screen
+    
+    if (setupRun == 0) {
+        setupRun = 1;
+        setup();
+    }
+}
+
+let cultivationMenuUnlocked = 0;
+function checkMilestones() {
+    if (magicalPower > 999) { //should  be higher
+        if (cultivationMenuUnlocked == 0) {
+            cultivationMenuButton.style.display = "grid";
+        }
+        cultivationMenuUnlocked = 1;
+    }
+}
+
+
+
+
+
+const trainingLevelInfo = document.getElementById("trainingLevel");
+const intensifyLevelInfo = document.getElementById("intensifyLevel");
+const multiplicationLevel = document.getElementById("multiplicationLevel");
+
+trainingLevelInfo.addEventListener("mouseover", () => {
+    createInfo(0, "Placeholder Info Text For Training", trainingLevelInfo)
+});
+
+
+trainingLevelInfo.addEventListener("mouseout", () => {
+    deleteInfo(0)
+});
+
+intensifyLevelInfo.addEventListener("mouseover", () => {
+    createInfo(1, "Placeholder Info Text For INTENSIFY", intensifyLevelInfo)
+});
+
+
+intensifyLevelInfo.addEventListener("mouseout", () => {
+    deleteInfo(1)
+});
+
+multiplicationLevel.addEventListener("mouseover", () => {
+    createInfo(2, "Placeholder Info Text For mult", multiplicationLevel)
+});
+
+
+multiplicationLevel.addEventListener("mouseout", () => {
+    deleteInfo(2)
+});
+
+
+
+const infoMap = new Map();
+
+//The creation of an element described below needs to be made next
+//This will be used for when you hover/click on a skill to open an info screen
+
+//Creates a div that displays provided info, this will be triggered from clicking on levelup buttons
+let currentVH = 0;
+let currentVW = 0;
+function calcViewportUnits() {
+    let viewportHeight = window.innerHeight;
+    let viewportWidth = window.innerWidth;
+    currentVH = viewportHeight / 100;
+    currentVW = viewportWidth / 100;
+}
+
+function createInfo(id, text, infoElement) {
+    //let divName = "info" + id //uuh idk if I need this anymore
+    const newDiv = document.createElement('div')
+    newDiv.className = "info";
+    newDiv.textContent = text;
+    calcViewportUnits();
+    let topVal = infoElement.getBoundingClientRect().top; //can consolodate these two, also 
+    let leftVal = infoElement.getBoundingClientRect().left;
+    newDiv.style.top = topVal - (currentVH * 25) + "px"; //offset - 10vh (height of level display)
+    newDiv.style.left = leftVal - (currentVW * 2.5) + "px";
+    infoMap.set(id, newDiv)
+    body.appendChild(infoMap.get(id))
+    //console.log("add");
+}
+
+//Deletes an info div, the id is relating to which things info you are viewing
+function deleteInfo(id) {
+    body.removeChild(infoMap.get(id));
+    infoMap.delete(id);
+    //console.log("del");
+}
