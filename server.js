996c76ade508653c470205c82f56a5c87708be41
sammillecchia <sammillecchia@gmail.com>
2025-08-24 18:54:53 -0400

    11 - messed the last one up

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use('/magic', express.static(path.join(__dirname, 'public')));
-
 app.use(express.static(path.join(__dirname, 'test')))
+
+app.use('/magic', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`Static server listening at http://localhost:${port}`);
